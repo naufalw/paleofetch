2d15273e2abbd3e5f981a7bb71d24fb0b3f441a4
Naufal Wiwit Putra <67492436+naufalw@users.noreply.github.com>
2021-08-22 22:55:19 +0700

    Update config.h

--- config.h
+++ config.h
@@ -10,17 +10,12 @@
     { "Host: ",       get_host,                true  }, \
     { "Kernel: ",     get_kernel,              true  }, \
     { "Uptime: ",     get_uptime,              false }, \
-    { "Battery: ",    get_battery_percentage,  false }, \
-    SPACER \
     { "Packages: ",   get_packages_pacman,   false }, \
     { "Shell: ",      get_shell,             false }, \
     { "Resolution: ", get_resolution,        false }, \
     { "Terminal: ",   get_terminal,          false }, \
-    SPACER \
-    { "CPU: ",        get_cpu,                 true  }, \
     { "GPU: ",        get_gpu1,                true  }, \
     { "Memory: ",     get_memory,              false }, \
-    SPACER \
     { "",             get_colors1,             false }, \
     { "",             get_colors2,             false }, \
 }
